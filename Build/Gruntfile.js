7eeab20825d4fce740204e604004c95dc0aeb9cf
Bernd Fallert <bernd.fallert@uni-mannheim.de>
2026-07-27 17:28:02 +0200

    switch off sourceMap

--- Build/Gruntfile.js
+++ Build/Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         less: {
             development: {
                 options: {
-                    sourceMap: true,
+                    sourceMap: false,
                     compress: false,
                     yuicompress: false,
                     optimization: 2
