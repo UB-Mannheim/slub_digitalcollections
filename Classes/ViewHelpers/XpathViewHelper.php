4c1fe436d3e90c828d41fe8e6787eb778d917a3d
BFallert <bernd.fallert@bib.uni-mannheim.de>
2026-09-08 13:39:38 +0200

    fix: use getMets() accessor instead of protected $mets property (upstream #2076 removed magicGetMets)

--- Classes/ViewHelpers/XpathViewHelper.php
+++ Classes/ViewHelpers/XpathViewHelper.php
@@ -126,12 +126,16 @@ class XpathViewHelper extends AbstractViewHelper
 
         /** @var MetsDocument $currentDocument */
         $currentDocument = $document->getCurrentDocument();
-        $currentDocument->mets->registerXPathNamespace('mets', 'http://www.loc.gov/METS/');
-        $currentDocument->mets->registerXPathNamespace('mods', 'http://www.loc.gov/mods/v3');
-        $currentDocument->mets->registerXPathNamespace('dv', 'http://dfg-viewer.de/');
-        $currentDocument->mets->registerXPathNamespace('slub', 'http://slub-dresden.de/');
+        $mets = $currentDocument->getMets();
+        if ($mets === null) {
+            return;
+        }
+        $mets->registerXPathNamespace('mets', 'http://www.loc.gov/METS/');
+        $mets->registerXPathNamespace('mods', 'http://www.loc.gov/mods/v3');
+        $mets->registerXPathNamespace('dv', 'http://dfg-viewer.de/');
+        $mets->registerXPathNamespace('slub', 'http://slub-dresden.de/');
 
-        $result = $currentDocument->mets->xpath($xpath);
+        $result = $mets->xpath($xpath);
 
         if ($returnArray) {
             $output = [];
